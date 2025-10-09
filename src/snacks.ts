3404e132201f944f3a43c731b7af56a97acd4b41
Andrew Jeter <ajeter@ucsd.edu>
2025-10-09 11:29:58 -0700

    added snacks file with printable list to console

---
+++ src/snacks.ts
@@ -0,0 +1,12 @@
+const snacks: string[] = ['chips', 'pringles', 'pretzels'];
+
+export function printSnacks(): void {
+	for (const s of snacks) {
+		console.log(s);
+	}
+}
+
+// call the function to print the snacks
+printSnacks();
+
+export default snacks;
