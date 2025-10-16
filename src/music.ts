6ce0714df1232a1a6a8c3df3999c4e3ab4dd7dc1
Starsun Howe <sahowe@ucsd.edu>
2025-10-16 10:40:30 -0700

    Added all final music

--- src/music.ts
+++ src/music.ts
@@ -1,5 +1,6 @@
 const artists: string[] = ['Taylor Swift', 'Ed Sheeran', 'Adele', 
-     'Drake', 'Billie Eilish', 'The Weeknd'];
+     'Drake', 'Billie Eilish', 'The Weeknd', 'Playboi Carti',
+	 'Dua Lipa', 'Olivia Rodrigo'];
 
 export function printArtists(): void {
 	for (const a of artists) {
